470e419599ff9daedd73868bc40e46ab57d1b7f4
David <yidavidh@outlook.com>
2023-04-24 19:47:51 -0700

    Lab Complete

--- index.js
+++ index.js
@@ -1,1 +1,58 @@
 // Add your code here
+const formData = {
+  dogName: "Byron",
+  dogBreed: "Poodle",
+};
+
+const configurationObject = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json",
+  },
+  body: JSON.stringify(formData),
+};
+
+fetch("http://localhost:3000/dogs", configurationObject)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (object) {
+    console.log(object);
+  });
+
+function submitData(name, email) {
+  const data = {
+    name: name,
+    email: email
+  };
+
+  const url = 'http://localhost:3000/users';
+  const headers = {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
+  };
+
+  return fetch(url, {
+    method: 'POST',
+    headers: headers,
+    body: JSON.stringify(data)
+  })
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => {
+    const id = data.id;
+    const idElement = document.createElement('p');
+    idElement.textContent = `New ID: ${id}`;
+    document.body.appendChild(idElement);
+  })
+  .catch(error => {
+    const errorElement = document.createElement('p');
+    errorElement.textContent = `Error: ${error.message}`;
+    document.body.appendChild(errorElement);
+  });
+}
